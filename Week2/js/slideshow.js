86b22407ea6a18e5d1c19a8309c1d7fa3f68f5bf
Adrian O'Grady <elpollouk@gmail.com>
2016-06-13 23:10:16 +0100

    Style tweaks and added image list

--- Week2/js/slideshow.js
+++ Week2/js/slideshow.js
@@ -1,5 +1,18 @@
-function main() {
-    console.log("Hello world!");
+var imagePaths = [
+    "images/Docked.jpg",
+    "images/EarlyMorning.jpg",
+    "images/EuropaVista.jpg",
+    "images/NightTimeLanding.jpg",
+    "images/Srv.jpg",
+    "images/SrvAndShip.jpg"
+];
 
-    viewerImg.src = "images/Docked.jpg";
+function setImage(index) {
+    viewerImg.src = imagePaths[index];
+}
+
+function main() {
+    console.log("Hello World!");
+
+    setImage(0);
 }
